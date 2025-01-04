ef02fa2c45ceef4f5ad37b1455cee0f2977baee5
elmogdadhassan1991@gmail.com <elmogdadhassan1991@gmail.com>
2025-01-04 11:35:30 +0300

    Create Users Schema and logic register user in DB

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import cookieParser from 'cookie-parser';
 
 //Utiles
 import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -14,13 +15,11 @@ connectDB();
 
 const app = express();
 
-// app.use(express.json());
-// app.use(express.urlencoded({extended : true}));
-// app.use(cookieParser);
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(cookieParser());
 
-app.get("/" , (req, res) => {
-    res.send("Hello World")
-})
+app.use('/api/users', userRoutes);
 
 
 app.listen(port, () => console.log(`Server running on port : ${port}`))
