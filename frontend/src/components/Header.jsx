e4738a9a978a20a480a4f04771cbbca6d9e29bac
elmogdadhassan1991@gmail.com <elmogdadhassan1991@gmail.com>
2025-01-04 08:56:01 +0300

    Install Packages and Create Folders Frontend and Backend

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import "./Header.css";
+
+function Header() {
+  return 
+  (
+     <>
+    <ul>
+      <li>
+        <Link>Home</Link>
+      </li>
+      <li>
+        <Link>About</Link>
+      </li>
+      <li>
+        <Link>Contact</Link>
+      </li>
+    </ul>
+  </>
+  )
+}
+
+export default Header;
