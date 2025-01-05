713ac7ab28ea0c6968292b200b17d421e9dd0300
elmogdadhassan1991@gmail.com <elmogdadhassan1991@gmail.com>
2025-01-05 10:00:51 +0300

    Create Authorization and Logout , get current user profile and update current user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -57,5 +57,71 @@ const loginUser = asyncHandler(async (req, res) => {
           return; 
         }
     }
+});
+
+//// Logout 
+const logoutCurrentUser = asyncHandler(async (req, res) => {
+    res.cookie('jwt' , '' , {
+        httpOnly: true,
+        expires: new Date(0)
+    });
+    res.status(200).json({message : 'Logged Successfully'})
 })
-export {createUser, loginUser}
+
+
+/// Get All Users
+
+const getAllUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({});
+    res.json(users)
+})
+
+/// Get Profile
+
+const getCurrentUserProfile = asyncHandler (async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            username : user.username,
+            email : user.email
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+/// Update Profile
+
+const updateCurrentUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        user.username = req.body.username || user.username;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            const salt = await bcrypt.genSalt(10);
+            const handlePassword = await bcrypt.hash(req.body.password, salt)
+         
+            user.password = handlePassword
+        }
+
+        const updatedUser = await user.save();
+
+        res.json({
+            _id: updatedUser._id,
+            email : updatedUser.email,
+            username : updatedUser.username,
+            isAdmin : updatedUser.isAdmin
+        });
+    } else {
+        res.status(404)
+        throw new Error("User not found")
+    }
+})
+
+
+export {createUser, loginUser, logoutCurrentUser, getAllUsers, getCurrentUserProfile,updateCurrentUserProfile}
